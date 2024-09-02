4a39599d58e30ed16c649270e94cc6a530a6460c
Nicou11 <lyoungin1204@gmail.com>
2024-09-02 14:14:26 +0900

    up ver.

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
 from src.fishmlserv.model.manager import get_model_path
+from sklearn.neighbors import KNeighborsClassifier
 import pickle
 import os
 
@@ -29,25 +30,25 @@ def fish(length: float, weight: float):
     ```
     """
     ### 모델 불러오기
-    with open(get_model_path, "rb") as f:
+    with open(get_model_path(), "rb") as f:
         fish_model = pickle.load(f)
+
+    prediction = fish_model.predict([[length, weight]])
     
-    if kn.predict([[30, 600]])[0] == 1:
-        print('도미')
-    else:
-        print('빙어')
+    #if kn.predict([[30, 600]])[0] == 1:
+     #   print('도미')
+    #else:
+     #   print('빙어')
     #prediction = fish_model.predict([[length, weight]])
 
     #fish_class = "몰라"
-    #if prediction[0] == 1:
-    #    fish_class = "도미"
-    #else:
-    #  fish_class = "빙어"
+    if prediction[0] == 1:
+        fish_class = "도미"
+    else:
+      fish_class = "빙어"
 
     return {
-                "prediction": fish_class,
-                "length": length, 
-                "weight": weight
+                "생선": fish_class,
             }
 
 @app.get("/model_path")
